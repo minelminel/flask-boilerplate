1ff98d12bc50d44b5c68cbded36bbf03ee3dac47
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-09-10 02:01:17 -0400

    dumb mistake

--- resources/index.py
+++ resources/index.py
@@ -1,7 +1,8 @@
+from flask import current_app
 from flask import request, jsonify
 from flask_restful import Resource
 
 
 class IndexResource(Resource):
     def get(self):
-        return jsonify(dict(app.config))
+        return jsonify(dict(current_app.config))
