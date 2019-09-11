b1164c129875211caac8c53feb48ea99f5cf46f4
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-09-11 03:36:21 -0400

    added ability to view git sha and environment label

--- src/webapp/resources/index.py
+++ src/webapp/resources/index.py
@@ -1,8 +1,27 @@
 from flask import current_app
 from flask import request, jsonify
 from flask_restful import Resource
+from operator import attrgetter
 
 
 class IndexResource(Resource):
+
+    @staticmethod
+    def routes_command(sort='rule', all_methods=False):
+        reply = []
+        rules = list(current_app.url_map.iter_rules())
+        if not rules:
+            return reply
+        ignored_methods = set(() if all_methods else ('HEAD', 'OPTIONS'))
+        if sort in ('endpoint', 'rule'):
+            rules = sorted(rules, key=attrgetter(sort))
+        rule_methods = [','.join(sorted(rule.methods - ignored_methods)) for rule in rules]
+        for rule, methods in zip(rules, rule_methods):
+            if (rule.endpoint != 'static') and ('dashboard' not in rule.endpoint):
+                reply.append(dict(endpoint=rule.endpoint, methods=methods.split(','), rule=rule.rule))
+        return reply
+
+
     def get(self):
-        return str(current_app.config)
+        routes = self.routes_command()
+        return jsonify(routes)
